e43b1ddcc78e42d7048146bdbec9915a07c44cd3
Thien Nguyen <94078395+Thienguen@users.noreply.github.com>
2022-10-13 02:05:23 -0700

    a

---
+++ Data Structure/Sort/bubbleSort/main.cpp
@@ -0,0 +1,66 @@
+#include <SDL2/SDL.h>
+#include "math.h"
+#include <random>
+#include <algorithm>
+#include <iostream>
+
+// #define main SDL_main
+
+void draw_state(std::vector<int> &v, SDL_Renderer *renderer, long red, long blue)
+{
+    int current_x = 0;
+    for (int i : v)
+    {
+        if (current_x == red)
+            SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        else if (current_x == blue)
+            SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
+        else
+            SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+
+        // SDL_RenderDrawLine(renderer, current_x, 99, current_x, i);
+        SDL_RenderDrawLine(renderer, current_x, i, current_x, i);
+        current_x += 1;
+    }
+}
+
+int main(int argv, char** args)
+{
+    std::random_device rd;
+    std::uniform_int_distribution<int> d(1, 99);
+    std::vector<int> v;
+    for (int i = 0; i < 100; i++)
+    {
+        v.push_back(d(rd));
+    }
+
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    SDL_CreateWindowAndRenderer(100 * 10, 100 * 10, 0, &window, &renderer);
+    SDL_RenderSetScale(renderer, 10, 10);
+
+    for (unsigned int i = 0; i < v.size(); i++)
+    {
+        for (unsigned int j = i; j < v.size(); j++)
+        {
+            if (v[j] < v[i])
+            {
+                std::swap(v[j], v[i]);
+            }
+            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+            SDL_RenderClear(renderer);
+            draw_state(v, renderer, j, i);
+            SDL_RenderPresent(renderer);
+            SDL_Delay(4);
+        }
+    }
+	
+    for (int i : v)
+    {
+        std::cout << i << " ";
+    }
+
+    std::cout << "\n";
+
+    SDL_Delay(2000);
+}
---
+++ Data Structure/SortVisualization/SortingVisualizationTutorial-master/Tutorial/Makefile
@@ -0,0 +1,2 @@
+default:
+	g++ examplefile.cpp -o examplefile -lraylib -lGL -lm -lpthread -ldl -lm -lpthread -ldl -lrt -lX11
